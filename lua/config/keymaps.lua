930386810a36273cd0b78e2bebec8abe4f79337b
Elias Ebner <eliase229@gmail.com>
2025-05-17 21:26:20 +0200

    added descriptions to kemyaps, added code actions keymap, added harpoon.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,10 +6,21 @@ vim.keymap.set("n", "<space>x", require("oil").open)
 
 ----------------------------- LSP -----------------------------
 -- GO TO DEFINITION
-vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {
+    desc = "Go to the definition of the symbol under the cursor."
+})
 
 -- GO TO REFERENCES
-vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "gr", vim.lsp.buf.references, {
+    desc = "Show the references of the symbol under the cursor."
+})
 
 -- RENAME
-vim.keymap.set("n", "grn", vim.lsp.buf.rename)
+vim.keymap.set("n", "grn", vim.lsp.buf.rename, {
+    desc = "Rename the symbol under the cursor."
+})
+
+-- CODE ACTIONS
+vim.keymap.set("n", "<space>a", vim.lsp.buf.code_action, {
+    desc = "Open the code actions menu."
+})
