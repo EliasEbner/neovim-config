a00fd94572c78c1311dbadf55395d64e37897ad2
Elias Ebner <eliase229@gmail.com>
2025-05-17 21:52:10 +0200

    harpoon setup with some basic keybindings and telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,15 @@
 -- TOGGLE SIDEBAR
-vim.keymap.set("n", "<space>s", require("sidebar-nvim").toggle)
+vim.keymap.set("n", "<space>s", require("sidebar-nvim").toggle, {
+    desc = "Toggle the sidebar."
+})
 
 -- OPEN FILE EXPLORER
-vim.keymap.set("n", "<space>x", require("oil").open)
+vim.keymap.set("n", "<space>x", require("oil").open, {
+    desc = "Open the file explorer."
+})
+
+-- TELESCOPE FIND FILES
+vim.keymap.set("n", "<space>f", require("telescope.builtin").find_files)
 
 ----------------------------- LSP -----------------------------
 -- GO TO DEFINITION
@@ -24,3 +31,34 @@ vim.keymap.set("n", "grn", vim.lsp.buf.rename, {
 vim.keymap.set("n", "<space>a", vim.lsp.buf.code_action, {
     desc = "Open the code actions menu."
 })
+
+
+----------------------------- HARPOON -----------------------------
+-- ADD FILE TO HARPOON LIST
+vim.keymap.set("n", "<space>ha", function()
+    require("harpoon"):list():add()
+end)
+
+-- REMOVE FILE FROM HARPOON LIST
+vim.keymap.set("n", "<space>hd", function()
+    require("harpoon"):list():remove()
+end)
+
+-- OPEN HARPOON LIST
+vim.keymap.set("n", "<space>hl", function()
+    require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+end)
+
+-- GO TO SPECIFIC LIST ITEMS
+vim.keymap.set("n", "<space>hh", function()
+    require("harpoon"):list():select(1)
+end)
+vim.keymap.set("n", "<space>ht", function()
+    require("harpoon"):list():select(2)
+end)
+vim.keymap.set("n", "<space>hn", function()
+    require("harpoon"):list():select(3)
+end)
+vim.keymap.set("n", "<space>hs", function()
+    require("harpoon"):list():select(4)
+end)
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,5 +1,8 @@
 return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require("harpoon").setup()
+    end
 }
