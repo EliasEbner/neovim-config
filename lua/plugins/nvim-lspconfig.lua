747f7e131c7145ae2458486dc74fbadf58d79352
Elias Ebner <eliase229@gmail.com>
2025-05-22 20:06:18 +0200

    some lsp stuff

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,13 +1,15 @@
 return {
-    "neovim/nvim-lspconfig",
-    opts = {},
-    config = function()
-        vim.lsp.enable("lua_ls")
-        vim.lsp.enable("pyright")
-        vim.lsp.enable("rust_analyzer")
-        vim.lsp.enable("jdtls")
-        vim.lsp.enable("ts_ls")
-        vim.lsp.enable("tailwindcss")
-        vim.lsp.enable("eslint")
-    end
+  "neovim/nvim-lspconfig",
+  opts = {},
+  config = function()
+    vim.lsp.enable("lua_ls")
+    vim.lsp.enable("pyright")
+    vim.lsp.enable("rust_analyzer")
+    vim.lsp.enable("jdtls")
+    vim.lsp.enable("ts_ls")
+    vim.lsp.enable("tailwindcss")
+    vim.lsp.enable("eslint")
+    vim.lsp.enable("html")
+    vim.lsp.enable("emmet_language_server")
+  end
 }
