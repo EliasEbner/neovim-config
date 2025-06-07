e5dc000cbd0793df5514d9f45a78805c6d1d02b9
Elias Ebner <elebner@unibz.it>
2025-06-07 02:37:34 +0200

    tried adding tailwind for rust leptos but it seems not to be working

--- lua/config/format-on-save.lua
+++ lua/config/format-on-save.lua
@@ -3,6 +3,21 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*.*',
   group = 'AutoFormatting',
   callback = function()
-    vim.lsp.buf.format({ async = false })
+    local filetype = vim.bo.filetype
+    -- List of filetypes where we want to use Prettier instead of LSP formatting
+    local prettier_filetypes = {
+      'javascript',
+      'javascriptreact',
+      'typescript',
+      'typescriptreact',
+      'tsx',
+      'jsx',
+    }
+
+    if vim.tbl_contains(prettier_filetypes, filetype) then
+      vim.cmd('Prettier')
+    else
+      vim.lsp.buf.format({ async = false })
+    end
   end,
 })
